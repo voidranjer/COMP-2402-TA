f63ea37692eb68ef4c0d7d29d5b38a428c4fb30b
James <logicism81@gmail.com>
2023-09-14 19:11:26 -0400

    pre redo p7

--- a1/comp2402a1/Part7.java
+++ a1/comp2402a1/Part7.java
@@ -6,22 +6,57 @@ import java.io.FileWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.PrintWriter;
+import java.util.ArrayList;
 
 public class Part7 {
-	
+
 	/**
 	 * Your code goes here - see Part0 for an example
+	 * 
 	 * @param r the reader to read from
 	 * @param w the writer to write to
 	 * @throws IOException
 	 */
 	public static void doIt(BufferedReader r, PrintWriter w) throws IOException {
-		// Your code goes here - see Part0 for an example
+		ArrayList<String[]> blocks = new ArrayList<>();
+
+		int blockLength = 1;
+		int currLengthPos = 0;
+		String[] block = new String[blockLength];
+
+		for (String line = r.readLine(); line != null; line = r.readLine()) {
+			if (currLengthPos >= blockLength) {
+				blocks.add(block);
+				blockLength++;
+				block = new String[blockLength];
+				block[0] = line; // possibly check for reset line here
+				currLengthPos = 1;
+			}
+
+			block[currLengthPos] = line;
+			currLengthPos++;
+			if (line.compareTo("***reset***") == 0) {
+				blocks.add(block);
+				blockLength = 1;
+				block = new String[blockLength];
+				currLengthPos = 1;
+			}
+
+		}
+
+		for (int i = blocks.size() - 1; i >= 0; i--) {
+			block = blocks.get(i);
+			for (int j = 0; j < block.length; j++) {
+				w.println(block[j]);
+			}
+		}
 	}
 
 	/**
-	 * The driver.  Open a BufferedReader and a PrintWriter, either from System.in
-	 * and System.out or from filenames specified on the command line, then call doIt.
+	 * The driver. Open a BufferedReader and a PrintWriter, either from System.in
+	 * and System.out or from filenames specified on the command line, then call
+	 * doIt.
+	 * 
 	 * @param args
 	 */
 	public static void main(String[] args) {
@@ -33,7 +68,7 @@ public class Part7 {
 				w = new PrintWriter(System.out);
 			} else if (args.length == 1) {
 				r = new BufferedReader(new FileReader(args[0]));
-				w = new PrintWriter(System.out);				
+				w = new PrintWriter(System.out);
 			} else {
 				r = new BufferedReader(new FileReader(args[0]));
 				w = new PrintWriter(new FileWriter(args[1]));
@@ -42,7 +77,7 @@ public class Part7 {
 			doIt(r, w);
 			w.flush();
 			long stop = System.nanoTime();
-			System.out.println("Execution time: " + 1e-9 * (stop-start));
+			System.out.println("Execution time: " + 1e-9 * (stop - start));
 		} catch (IOException e) {
 			System.err.println(e);
 			System.exit(-1);
