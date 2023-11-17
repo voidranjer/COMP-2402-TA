90d6919959d9d64e9feca1da770a97b8da7f8197
James <logicism81@gmail.com>
2023-11-16 23:42:37 -0500

    Trying to test kSum

--- a4/comp2402a4/CustomTester.java
+++ a4/comp2402a4/CustomTester.java
@@ -6,20 +6,14 @@ public class CustomTester {
     UltraFast stack = new UltraFast();
 
     // Push some values onto the stack
-    int[] values = { 3, 5, 2, 7, 1, 4, 6 };
+    int[] values = { 3, 5, 2 };
     for (int value : values) {
       stack.push(value);
     }
 
-    // Test the visualizeKSum method
-    testVisualizeKSum(stack, 3);
+    for (int i = 0; i < values.length; i++) {
+      System.out.println(stack.pop());
+    }
   }
 
-  public static void testVisualizeKSum(UltraFast stack, int k) {
-    System.out.println("Initial Stack:");
-    stack.display();
-
-    System.out.println("\nCalling visualizeKSum with k = " + k);
-    UltraFast.visualizeKSum(stack, k);
-  }
 }
--- a4/comp2402a4/UltraFast.java
+++ a4/comp2402a4/UltraFast.java
@@ -1,192 +1,144 @@
 package comp2402a4;
 
-import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.Iterator;
-import java.util.PrimitiveIterator;
-
-import comp2402a4.UltraStack;
+import java.util.ArrayList;
 
 public class UltraFast implements UltraStack {
-  // TODO: Your data structures go here
+  final static int INITIAL_SIZE = 7;
+  final static int INITIAL_HEIGHT = 3;
 
-  final static int INITIAL_HEIGHT = 4;
-
+  ArrayList<Integer> sumHeap;
+  int stackStart; // index of first element in stack (first element on the bottom level)
   int height;
-  int n;
+  int size;
 
-  int[] maxRows;
-  long[] sumRows;
+  // Get number of nodes present on the level 'h'
+  private int getNumNodes(int h) {
+    return (int) Math.pow(2, h - 1);
+  }
 
-  int listBegin;
+  // Get index of parent
+  private int getParentIndex(int i) {
+    return (i - 1) / 2; // implicit floor
+  }
+
+  // Get index of left child
+  private int getLeftChildIndex(int i) {
+    return 2 * i + 1;
+  }
+
+  // Get index of right child
+  private int getRightChildIndex(int i) {
+    return 2 * i + 2;
+  }
 
   public UltraFast() {
-    // TODO: Your code goes here
+    size = 0;
     height = INITIAL_HEIGHT;
 
-    n = 0;
-    maxRows = new int[(int) Math.pow(2, height)];
-    sumRows = new long[(int) Math.pow(2, height)];
-    listBegin = (int) Math.pow(2, height - 1) - 1; // bottom row, pos 0
-
-  }
-
-  /*
-   * Call after changing maxRows[0][i] and sumRows[0][i]
-   * Will update all nodes that need to be updated
-   * Input n as position in stack
-   */
-  private void updateRows(int i) {
-    int currentIndex = listBegin + i;
-
-    while (currentIndex > 0) {
-
-      // formula for index of parent node (in Alexa's notes)
-      currentIndex = (currentIndex - 1) / 2;
-
-      maxRows[currentIndex] = Math.max(maxRows[2 * currentIndex + 1], maxRows[2 * currentIndex + 2]);
-      sumRows[currentIndex] = sumRows[2 * currentIndex + 1] + sumRows[2 * currentIndex + 2];
+    sumHeap = new ArrayList<Integer>();
+    for (int i = 0; i < INITIAL_SIZE; i++) {
+      sumHeap.add(0);
     }
 
+    stackStart = sumHeap.size() - getNumNodes(height);
   }
-
-  // public void display() {
-  // System.out.println("Max rows:");
-  // for (int h = height - 1; h >= 0; h--) {
-  // System.out.println();
-  // for (int c = (int) Math.pow(2, h - 1) - 1; c < (int) Math.pow(2, h); c++) {
-  // System.out.print(maxRows[c]);
-  // }
-  // }
-  // System.out.println();
-  // System.out.println("Sum rows:");
-  // for (int h = height - 1; h >= 0; h--) {
-  // System.out.println();
-  // for (int c = (int) Math.pow(2, h - 1) - 1; c < (int) Math.pow(2, h); c++) {
-  // System.out.print(sumRows[c]);
-  // }
-  // }
-  // System.out.println();
-  // }
 
   public void push(int x) {
-    // TODO: Your code goes here
-    if (listBegin + n + 2 >= maxRows.length) {
-      expand();
+    int targetIndex = stackStart + size;
+
+    sumHeap.set(targetIndex, x);
+
+    // traverse up the tree and update the sum
+    int parentIndex = getParentIndex(targetIndex);
+    while (parentIndex > 0) {
+      sumHeap.set(parentIndex, sumHeap.get(parentIndex) + x);
+      parentIndex = getParentIndex(parentIndex);
     }
 
-    maxRows[listBegin + n] = x;
-    sumRows[listBegin + n] = x;
-    n++;
-    maxRows[listBegin + n] = Integer.MIN_VALUE;
-    updateRows(n - 1);
+    size++;
   }
 
   public Integer pop() {
-    if (n == 0)
+    if (size == 0) {
       return null;
-    n--;
-    Integer x = maxRows[listBegin + n];
-    maxRows[listBegin + n] = Integer.MIN_VALUE;
-    sumRows[listBegin + n] = 0;
-    updateRows(n);
-    return x;
+    }
 
+    int targetIndex = stackStart + size - 1;
+    int targetValue = sumHeap.get(targetIndex);
+
+    // traverse up the tree and update the sum
+    int parentIndex = getParentIndex(targetIndex);
+    while (parentIndex > 0) {
+      sumHeap.set(parentIndex, sumHeap.get(parentIndex) - targetValue);
+      parentIndex = getParentIndex(parentIndex);
+    }
+
+    size--;
+
+    return targetValue;
   }
 
   public Integer get(int i) {
-    if (i < 0 || i >= n)
-      return null;
-
-    return maxRows[listBegin + i];
+    return sumHeap.get(stackStart + i);
   }
 
   public Integer set(int i, int x) {
-    if (i < 0 || i >= n)
-      return null;
+    int targetIndex = stackStart + i;
+    int targetValue = sumHeap.get(targetIndex);
 
-    Integer prev = maxRows[listBegin + i];
-    maxRows[listBegin + i] = x;
-    sumRows[listBegin + i] = x;
+    // traverse up the tree and update the sum
+    int parentIndex = getParentIndex(targetIndex);
+    while (parentIndex > 0) {
+      sumHeap.set(parentIndex, sumHeap.get(parentIndex) - targetValue + x);
+      parentIndex = getParentIndex(parentIndex);
+    }
 
-    updateRows(i);
-    // updateRows(i+1);
-    // updateRows(i-1);
-
-    return prev;
+    return targetValue;
   }
 
   public Integer max() {
-    if (n == 0)
-      return null;
-    return maxRows[0];
+    // TODO: Your code goes here
+    return null;
   }
 
+  // Sum of the last k elements in the stack
   public long ksum(int k) {
-    if (k >= n)
-      return sumRows[0];
-    if (k <= 0)
+    if (size == 0) {
       return 0;
+    }
 
-    int currentIndex = 0;
-    long sum = 0;
+    if (k >= size) {
+      return sumHeap.get(0);
+    }
 
-    int numZeros = (int) Math.pow(2, height - 1) - n;
-
-    k += numZeros;
-
-    int nodesOnRight = (int) Math.pow(2, height - 2);
-    while (k > 0) {
-      if (k >= nodesOnRight) {
-        k -= nodesOnRight;
-        sum += sumRows[currentIndex * 2 + 2]; // Add right
-        currentIndex = currentIndex * 2 + 1; // Go to left
-      } else {
-        currentIndex = currentIndex * 2 + 2; // Go to right
+    /*
+     * Traverse to the top of the tree, add to sum each time we make a right turn to
+     * get to parent
+     */
+    int currIndex = stackStart + size - k;
+    long sum = sumHeap.get(currIndex);
+    int parentIndex = getParentIndex(currIndex);
+    while (currIndex >= 0) {
+      /*
+       * If we made a right turn to get to parent (meaning that we're on the left
+       * child right now), add the right child's value to the sum
+       */
+      if (getLeftChildIndex(parentIndex) == currIndex) {
+        sum += sumHeap.get(getRightChildIndex(parentIndex)); // or, just do `sum += sumHeap.get(currIndex + 1)`
       }
-
-      nodesOnRight /= 2;
+      parentIndex = getParentIndex(currIndex);
     }
 
     return sum;
   }
 
   public int size() {
-    return n;
-  }
-
-  private void expand() {
-
-    int newHeight = height + 1;
-    int[] newMaxRows = new int[(int) Math.pow(2, newHeight)];
-    long[] newSumRows = new long[(int) Math.pow(2, newHeight)];
-    int newListBegin = (int) Math.pow(2, newHeight - 1) - 1;
-
-    int size = 1;
-    int pos = 0;
-    for (int h = height - 1; h >= 0; h--) {
-      System.arraycopy(maxRows, pos, newMaxRows, 2 * pos + 1, size);
-      System.arraycopy(sumRows, pos, newSumRows, 2 * pos + 1, size);
-      size *= 2;
-      pos = 2 * pos + 1;
-    }
-
-    listBegin = newListBegin;
-    height = newHeight;
-    maxRows = newMaxRows;
-    sumRows = newSumRows;
-
-    maxRows[0] = Math.max(maxRows[1], maxRows[2]);
-    sumRows[0] = sumRows[1] + sumRows[2];
-
+    return size;
   }
 
   public Iterator<Integer> iterator() {
-    ArrayList<Integer> a = new ArrayList<>();
-    for (int index = listBegin; index < listBegin + n; index++) {
-      a.add(maxRows[index]);
-    }
-    return a.iterator();
+    // TODO: Your code goes here
+    return null;
   }
-
 }
